83c091d10cb001a192c6458bc233f64e7d00706e
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2015-07-09 19:51:25 +0000

    -Consulta de precios e tablas

--- proteoerp/system/application/views/view_lprecios.php
+++ proteoerp/system/application/views/view_lprecios.php
@@ -7,8 +7,23 @@
 	<!--<a href="#presumen" class="ui-btn ui-icon-bars ui-btn-icon-left ui-shadow-icon" >Ver Res&uacute;men</a>-->
 	<form class="ui-filterable">
 		<input id="autocomplete-input" data-type="search" placeholder="Buscar art&iacute;culo...">
-		<ul id="autocomplete" data-role="listview" data-inset="true" data-filter="true" data-input="#autocomplete-input"></ul>
+		<ul id="autocomplete" data-role="listview" data-inset="true" data-filter="false" data-input="#autocomplete-input"></ul>
 	</form>
+
+
+	<table id='tabladata' data-role="table" class="ui-responsive table-stroke">
+		<thead>
+		<tr>
+			<th data-priority="2">C&oacute;digo</th>
+			<th>Descripci&oacute;n</th>
+			<th data-priority="3" style='text-align:right'>Precio</th>
+			<th data-priority="1" style='text-align:right'><abbr title="Rotten Tomato Rating">IVA</abbr></th>
+			<th data-priority="5" style='text-align:right'>Precio de Venta</th>
+		</tr>
+		</thead>
+		<tbody></tbody>
+	</table>
+
 
 </div>
 
@@ -18,50 +33,74 @@
 
 <script type="text/javascript">
 
+var actObj='';
+
 $(document).on("pagecreate", "#mainpage", function(){
 
 
-	$("#autocomplete").on("filterablebeforefilter", function(e, data){
-		var $ul    = $(this),
-			$input = $( data.input ),
-			value  = $input.val(),
-			html   = "";
+	$("#autocomplete-input").keyup(function() {
+
+		var $ul    = $("#tabladata tbody"),
+			$input = $(this),
+			value  = $input.val();
+		var xhr;
 		var base, precio;
-		$ul.html("");
+
 		if(value && value.length >= 4){
-			$ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
-			$ul.listview( "refresh" );
-			$.ajax({
+			if(typeof xhr == 'object'){
+				xhr.abort();
+			}
+
+			xhr = $.ajax({
 				url: "<?php echo site_url('inventario/lprecios/buscasinv'); ?>",
 				dataType: "json",
 				type: "POST",
 				crossDomain: false,
-				data: { q : $input.val() }
+				data: { q : value }
 			}).then(function(response){
-				$.each(response, function ( i, val ){
-console.log(val);
-					base   = val.base1;
-					precio = base*(1+(val.iva/100));
-					iva    = precio-base;
+				var a = JSON.stringify(response);
 
-					html += "<li><a href='#'>";
-					if(val.foto=='S'){
-						html += '<img src="<?php echo site_url('inventario/fotos/obtener'); ?>/'+val.id+'">';
-					}
-					html += "<h2>"+$('<span/>').text(val.descrip).html()+"";
-					html += "</h2>";
-					html += "<p>C&oacute;digo: <b>"+$('<span/>').text(val.codigo).html()+"</b> Precio: <b>"+nformat(base,2)+"</b> IVA: <b>"+nformat(iva,2)+"</b></p>";
-					html += "Precio de venta: <b style='font-size:1.2em'>"+nformat(precio,2)+"</b>";
-					html += "</li>";
-				});
-				console.log(html);
-				$ul.html(html);
-				$ul.listview( "refresh" );
-				$ul.trigger( "updatelayout");
+				console.log(response);
+				if(a !== actObj){
+					$ul.html("");
+					//$ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
+					//$ul.listview("refresh");
+
+					actObj = a;
+					var html   = "";
+					$.each(response, function ( i, val ){
+						base   = val.base1;
+						precio = base*(1+(val.iva/100));
+						iva    = precio-base;
+
+						//html += "<li><a href='#'>";
+						//if(val.foto=='S'){
+						//	html += '<img src="<?php echo site_url('inventario/fotos/obtener'); ?>/'+val.id+'">';
+						//}
+                        //
+						//html += "<h2>"+$('<span/>').text(val.descrip).html()+"";
+						//html += "</h2>";
+						//html += "<p>C&oacute;digo: <b>"+$('<span/>').text(val.codigo).html()+"</b> Precio: <b>"+nformat(base,2)+"</b> IVA: <b>"+nformat(iva,2)+"</b></p>";
+						//html += "Precio de venta: <b style='font-size:1.2em'>"+nformat(precio,2)+"</b>";
+
+						html += "<tr>";
+						html += "<td>"+$('<span/>').text(val.codigo).html()+"</td>";
+						html += "<td>"+$('<span/>').text(val.descrip).html()+"</td>";
+						html += "<td style='text-align:right'>"+nformat(base,2)+"</td>";
+						html += "<td style='text-align:right'>"+nformat(iva,2)+"</td>";
+						html += "<td style='text-align:right'><b style='font-size:1.2em'>"+nformat(precio,2)+"</b></td>";
+						html += "</tr>";
+
+						//html += "</a></li>";
+					});
+
+					$ul.html(html);
+					//$ul.listview( "refresh" );
+					//$ul.trigger( "updatelayout");
+				}
 			});
 		}
 	});
-
 
 	if($('#autocomplete-input').val()!=''){
 		$('#autocomplete-input').keyup();
