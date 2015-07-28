a6e5b31fe4dcdb14a7f9185d3075e44757eac6c0
datasql <datasql@gmail.com>
2015-07-28 15:47:23 +0000

    Medicos

---
+++ proteoerp/system/application/controllers/medico/medicos.php
@@ -0,0 +1,394 @@
+<?php
+/**
+* ProteoERP
+*
+* @autor    Andres Hocevar
+* @license  GNU GPL v3
+*/
+class Medicos extends Controller {
+	var $mModulo = 'RECURSOS DE ATENCION MEDICA';
+	var $titp    = 'RECURSOS DE ATENCION MEDICA';
+	var $tits    = 'MEDICOS';
+	var $url     = 'ventas/medicos/';
+
+	function Medicos(){
+		parent::Controller();
+		$this->load->library('rapyd');
+		$this->load->library('jqdatagrid');
+		$this->datasis->modulo_nombre( 'MEDICOS', $ventana=0, $this->titp  );
+	}
+
+	function index(){
+		$this->instalar();
+		/*if ( !$this->datasis->iscampo('medicos','id') ) {
+			$this->db->query('ALTER TABLE medicos DROP PRIMARY KEY');
+			$this->db->query('ALTER TABLE medicos ADD UNIQUE INDEX numero (numero)');
+			$this->db->query('ALTER TABLE medicos ADD COLUMN id INT(11) NULL AUTO_INCREMENT, ADD PRIMARY KEY (id)');
+		};*/
+		$this->datasis->creaintramenu(array('modulo'=>'173','titulo'=>'Medicos','mensaje'=>'Medicos','panel'=>'Salud','ejecutar'=>'ventas/medicos','target'=>'popu','visible'=>'S','pertenece'=>'1','ancho'=>900,'alto'=>600));
+		$this->datasis->modintramenu( 800, 600, substr($this->url,0,-1) );
+		redirect($this->url.'jqdatag');
+	}
+
+	//******************************************************************
+	// Layout en la Ventana
+	//
+	function jqdatag(){
+
+		$grid = $this->defgrid();
+		$param['grids'][] = $grid->deploy();
+
+		//Funciones que ejecutan los botones
+		$bodyscript = $this->bodyscript( $param['grids'][0]['gridname']);
+
+		//Botones Panel Izq
+		//$grid->wbotonadd(array("id"=>"funcion",   "img"=>"images/engrana.png",  "alt" => "Formato PDF", "label"=>"Ejemplo"));
+		$WestPanel = $grid->deploywestp();
+
+		$adic = array(
+			array('id'=>'fedita',  'title'=>'Agregar/Editar Registro'),
+			array('id'=>'fshow' ,  'title'=>'Mostrar Registro'),
+			array('id'=>'fborra',  'title'=>'Eliminar Registro')
+		);
+		$SouthPanel = $grid->SouthPanel($this->datasis->traevalor('TITULO1'), $adic);
+
+		$param['WestPanel']   = $WestPanel;
+		//$param['EastPanel'] = $EastPanel;
+		$param['SouthPanel']  = $SouthPanel;
+		$param['listados']    = $this->datasis->listados('MEDICOS', 'JQ');
+		$param['otros']       = $this->datasis->otros('MEDICOS', 'JQ');
+		$param['temas']       = array('proteo','darkness','anexos1');
+		$param['bodyscript']  = $bodyscript;
+		$param['tabs']        = false;
+		$param['encabeza']    = $this->titp;
+		$param['tamano']      = $this->datasis->getintramenu( substr($this->url,0,-1) );
+		$this->load->view('jqgrid/crud2',$param);
+	}
+
+	//******************************************************************
+	// Funciones de los Botones
+	//
+	function bodyscript( $grid0 ){
+		$bodyscript = '<script type="text/javascript">';
+		$ngrid = '#newapi'.$grid0;
+
+		$bodyscript .= $this->jqdatagrid->bsshow('medicos', $ngrid, $this->url );
+		$bodyscript .= $this->jqdatagrid->bsadd( 'medicos', $this->url );
+		$bodyscript .= $this->jqdatagrid->bsdel( 'medicos', $ngrid, $this->url );
+		$bodyscript .= $this->jqdatagrid->bsedit('medicos', $ngrid, $this->url );
+
+		//Wraper de javascript
+		$bodyscript .= $this->jqdatagrid->bswrapper($ngrid);
+
+		$bodyscript .= $this->jqdatagrid->bsfedita( $ngrid, '300', '400' );
+		$bodyscript .= $this->jqdatagrid->bsfshow( '300', '400' );
+		$bodyscript .= $this->jqdatagrid->bsfborra( $ngrid, '300', '400' );
+
+		$bodyscript .= '});';
+
+		$bodyscript .= '</script>';
+
+		return $bodyscript;
+	}
+
+	//******************************************************************
+	// Definicion del Grid o Tabla 
+	//
+	function defgrid( $deployed = false ){
+		$i      = 1;
+		$editar = "false";
+
+		$grid  = new $this->jqdatagrid;
+
+		$grid->addField('cedula');
+		$grid->label('Cedula');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 130,
+			'edittype'      => "'text'",
+			'editrules'     => '{ required:true}',
+			'editoptions'   => '{ size:13, maxlength: 13 }',
+		));
+
+
+		$grid->addField('nombre');
+		$grid->label('Nombre');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 200,
+			'edittype'      => "'text'",
+			'editrules'     => '{ required:true}',
+			'editoptions'   => '{ size:100, maxlength: 100 }',
+		));
+
+
+		$grid->addField('colegio');
+		$grid->label('Colegio');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 130,
+			'edittype'      => "'text'",
+			'editrules'     => '{ required:true}',
+			'editoptions'   => '{ size:13, maxlength: 13 }',
+		));
+
+
+		$grid->addField('sanidad');
+		$grid->label('Sanidad');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 130,
+			'edittype'      => "'text'",
+			'editrules'     => '{ required:true}',
+			'editoptions'   => '{ size:13, maxlength: 13 }',
+		));
+
+
+		$grid->addField('especialidad');
+		$grid->label('Especialidad');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 200,
+			'edittype'      => "'text'",
+			'editrules'     => '{ required:true}',
+			'editoptions'   => '{ size:100, maxlength: 100 }',
+		));
+
+
+		$grid->addField('id');
+		$grid->label('Id');
+		$grid->params(array(
+			'align'         => "'center'",
+			'frozen'        => 'true',
+			'width'         => 40,
+			'editable'      => 'false',
+			'search'        => 'false'
+		));
+
+
+		$grid->showpager(true);
+		$grid->setWidth('');
+		$grid->setHeight('290');
+		$grid->setTitle($this->titp);
+		$grid->setfilterToolbar(true);
+		$grid->setToolbar('false', '"top"');
+
+		$grid->setFormOptionsE('closeAfterEdit:true, mtype: "POST", width: 520, height:300, closeOnEscape: true, top: 50, left:20, recreateForm:true, afterSubmit: function(a,b){if (a.responseText.length > 0) $.prompt(a.responseText); return [true, a ];},afterShowForm: function(frm){$("select").selectmenu({style:"popup"});} ');
+		$grid->setFormOptionsA('closeAfterAdd:true,  mtype: "POST", width: 520, height:300, closeOnEscape: true, top: 50, left:20, recreateForm:true, afterSubmit: function(a,b){if (a.responseText.length > 0) $.prompt(a.responseText); return [true, a ];},afterShowForm: function(frm){$("select").selectmenu({style:"popup"});} ');
+		$grid->setAfterSubmit("$('#respuesta').html('<span style=\'font-weight:bold; color:red;\'>'+a.responseText+'</span>'); return [true, a ];");
+
+		$grid->setOndblClickRow('');		#show/hide navigations buttons
+		$grid->setAdd(    $this->datasis->sidapuede('MEDICOS','INCLUIR%' ));
+		$grid->setEdit(   $this->datasis->sidapuede('MEDICOS','MODIFICA%'));
+		$grid->setDelete( $this->datasis->sidapuede('MEDICOS','BORR_REG%'));
+		$grid->setSearch( $this->datasis->sidapuede('MEDICOS','BUSQUEDA%'));
+		$grid->setRowNum(30);
+		$grid->setShrinkToFit('false');
+
+		$grid->setBarOptions("addfunc: medicosadd, editfunc: medicosedit, delfunc: medicosdel, viewfunc: medicosshow");
+
+		#Set url
+		$grid->setUrlput(site_url($this->url.'setdata/'));
+
+		#GET url
+		$grid->setUrlget(site_url($this->url.'getdata/'));
+
+		if ($deployed) {
+			return $grid->deploy();
+		} else {
+			return $grid;
+		}
+	}
+
+	//******************************************************************
+	// Busca la data en el Servidor por json
+	//
+	function getdata(){
+		$grid       = $this->jqdatagrid;
+
+		// CREA EL WHERE PARA LA BUSQUEDA EN EL ENCABEZADO
+		$mWHERE = $grid->geneTopWhere('medicos');
+
+		$response   = $grid->getData('medicos', array(array()), array(), false, $mWHERE );
+		$rs = $grid->jsonresult( $response);
+		echo $rs;
+	}
+
+	//******************************************************************
+	// Guarda la Informacion del Grid o Tabla
+	//
+	function setData(){
+		$this->load->library('jqdatagrid');
+		$oper   = $this->input->post('oper');
+		$id     = $this->input->post('id');
+		$data   = $_POST;
+		$mcodp  = "??????";
+		$check  = 0;
+
+		unset($data['oper']);
+		unset($data['id']);
+		if($oper == 'add'){
+			if(false == empty($data)){
+				$check = $this->datasis->dameval("SELECT count(*) FROM medicos WHERE $mcodp=".$this->db->escape($data[$mcodp]));
+				if ( $check == 0 ){
+					$this->db->insert('medicos', $data);
+					echo "Registro Agregado";
+
+					logusu('MEDICOS',"Registro ????? INCLUIDO");
+				} else
+					echo "Ya existe un registro con ese $mcodp";
+			} else
+				echo "Fallo Agregado!!!";
+
+		} elseif($oper == 'edit') {
+			$nuevo  = $data[$mcodp];
+			$anterior = $this->datasis->dameval("SELECT $mcodp FROM medicos WHERE id=$id");
+			if ( $nuevo <> $anterior ){
+				//si no son iguales borra el que existe y cambia
+				$this->db->query("DELETE FROM medicos WHERE $mcodp=?", array($mcodp));
+				$this->db->query("UPDATE medicos SET $mcodp=? WHERE $mcodp=?", array( $nuevo, $anterior ));
+				$this->db->where("id", $id);
+				$this->db->update("medicos", $data);
+				logusu('MEDICOS',"$mcodp Cambiado/Fusionado Nuevo:".$nuevo." Anterior: ".$anterior." MODIFICADO");
+				echo "Grupo Cambiado/Fusionado en clientes";
+			} else {
+				unset($data[$mcodp]);
+				$this->db->where("id", $id);
+				$this->db->update('medicos', $data);
+				logusu('MEDICOS',"Grupo de Cliente  ".$nuevo." MODIFICADO");
+				echo "$mcodp Modificado";
+			}
+
+		} elseif($oper == 'del') {
+			$meco = $this->datasis->dameval("SELECT $mcodp FROM medicos WHERE id=$id");
+			//$check =  $this->datasis->dameval("SELECT COUNT(*) FROM medicos WHERE id='$id' ");
+			if ($check > 0){
+				echo " El registro no puede ser eliminado; tiene movimiento ";
+			} else {
+				$this->db->query("DELETE FROM medicos WHERE id=$id ");
+				logusu('MEDICOS',"Registro ????? ELIMINADO");
+				echo "Registro Eliminado";
+			}
+		};
+	}
+
+	//******************************************************************
+	// Edicion 
+
+	function dataedit(){
+		$this->rapyd->load('dataedit');
+		$script= '
+		$(function() {
+			$("#fecha").datepicker({dateFormat:"dd/mm/yy"});
+			$(".inputnum").numeric(".");
+		});
+		';
+
+		$edit = new DataEdit('', 'medicos');
+
+		$edit->script($script,'modify');
+		$edit->script($script,'create');
+		$edit->on_save_redirect=false;
+
+		$edit->back_url = site_url($this->url.'filteredgrid');
+
+		$edit->post_process('insert','_post_insert');
+		$edit->post_process('update','_post_update');
+		$edit->post_process('delete','_post_delete');
+		$edit->pre_process('insert', '_pre_insert' );
+		$edit->pre_process('update', '_pre_update' );
+		$edit->pre_process('delete', '_pre_delete' );
+
+		$edit->cedula = new inputField('Cedula','cedula');
+		$edit->cedula->rule='';
+		$edit->cedula->size =15;
+		$edit->cedula->maxlength =13;
+
+		$edit->nombre = new inputField('Nombre','nombre');
+		$edit->nombre->rule='';
+		$edit->nombre->size =50;
+		$edit->nombre->maxlength =100;
+
+		$edit->colegio = new inputField('Colegio','colegio');
+		$edit->colegio->rule='';
+		$edit->colegio->size =15;
+		$edit->colegio->maxlength =13;
+
+		$edit->sanidad = new inputField('Sanidad','sanidad');
+		$edit->sanidad->rule='';
+		$edit->sanidad->size =15;
+		$edit->sanidad->maxlength =13;
+
+		$edit->especialidad = new inputField('Especialidad','especialidad');
+		$edit->especialidad->rule='';
+		$edit->especialidad->size =50;
+		$edit->especialidad->maxlength =100;
+
+		$edit->build();
+
+		if($edit->on_success()){
+			$rt=array(
+				'status' =>'A',
+				'mensaje'=>'Registro guardado',
+				'pk'     =>$edit->_dataobject->pk
+			);
+			echo json_encode($rt);
+		}else{
+			echo $edit->output;
+		}
+	}
+
+	function _pre_insert($do){
+		$do->error_message_ar['pre_ins']='';
+		return true;
+	}
+
+	function _pre_update($do){
+		$do->error_message_ar['pre_upd']='';
+		return true;
+	}
+
+	function _pre_delete($do){
+		$do->error_message_ar['pre_del']='';
+		return false;
+	}
+
+	function _post_insert($do){
+		$primary =implode(',',$do->pk);
+		logusu($do->table,"Creo $this->tits $primary ");
+	}
+
+	function _post_update($do){
+		$primary =implode(',',$do->pk);
+		logusu($do->table,"Modifico $this->tits $primary ");
+	}
+
+	function _post_delete($do){
+		$primary =implode(',',$do->pk);
+		logusu($do->table,"Elimino $this->tits $primary ");
+	}
+
+	function instalar(){
+		if (!$this->db->table_exists('medicos')) {
+			$mSQL="CREATE TABLE `medicos` (
+			  `cedula` char(13) DEFAULT NULL,
+			  `nombre` char(100) DEFAULT NULL,
+			  `colegio` char(13) DEFAULT NULL COMMENT 'cm',
+			  `sanidad` char(13) DEFAULT NULL,
+			  `especialidad` char(100) DEFAULT NULL,
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Medicos, ayudantes y especialista'";
+			$this->db->query($mSQL);
+		}
+		//$campos=$this->db->list_fields('medicos');
+		//if(!in_array('<#campo#>',$campos)){ }
+	}
+}
+
+?>
